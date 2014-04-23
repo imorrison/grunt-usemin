8fe9ab70daf542a8de4944eabde6d81eb28973ca
Ian Morrison <ian@yola.com>
2014-04-23 10:38:11 -0700

    Fix style errors

--- test/test-fileprocessor.js
+++ test/test-fileprocessor.js
@@ -367,10 +367,10 @@ describe('FileProcessor', function () {
     });
 
     it('should replace unquoted image reference in inlined style', function () {
-          var content = '<li style="background: url(image.png);"></li>';
-          var replaced = fp.replaceWithRevved(content, ['app']);
-          assert.equal(replaced, '<li style="background: url(' + filemapping['app/image.png'] + ');"></li>');
-        });
+      var content = '<li style="background: url(image.png);"></li>';
+      var replaced = fp.replaceWithRevved(content, ['app']);
+      assert.equal(replaced, '<li style="background: url(' + filemapping['app/image.png'] + ');"></li>');
+    });
 
     it('should replace image reference in anchors', function () {
       var content = '<a href="image.png"></a>';
